77de2580122816562472c9066bd85142a4dfff7a
omkar2024 <omkarghadge2126@gmail.com>
2024-08-20 12:13:57 +0530

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   02_conditionals/07_solution.py
    	new file:   02_conditionals/08_solution.py
    	new file:   02_conditionals/09_solution.py
    	new file:   02_conditionals/10_solution.py

---
+++ 02_conditionals/10_solution.py
@@ -0,0 +1,9 @@
+pet_species = "Dog"
+pet_age = 1
+
+if(pet_species == "Dog" and pet_age <2):
+    print("Puppy Food")
+elif(pet_species == "Cat" and pet_age >5):
+    print("Senior cat Food")
+else:
+    print("Check the values")
